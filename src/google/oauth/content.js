f5ac4d48b9c25a1ec422ce83d504b89687873d87
KeyTey <k.terada.1211@gmail.com>
2020-09-24 01:20:20 +0900

    Update

--- src/google/oauth/content.js
+++ src/google/oauth/content.js
@@ -1,11 +1,16 @@
-// 許可ボタンをクリックする
-if (location.pathname === '/signin/oauth/consent') {
-    $('#submit_approve_access').click();
-}
-
-// 認証コードをコピーしてタブを閉じる
-if (location.pathname === '/o/oauth2/approval/v2/approvalnativeapp') {
-    $('.qBHUIf').select();
-    document.execCommand('copy');
-    window.close();
-}
+// メッセージイベント
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    // タブ更新以外の場合 -> キャンセル
+    if (message.type !== 'UPDATED') return;
+    if (message.data.status !== 'complete') return;
+    // 許可ボタンをクリックする
+    if (location.pathname === '/signin/oauth/consent') {
+        $('#submit_approve_access').click();
+    }
+    // 認証コードをコピーしてタブを閉じる
+    if (location.pathname === '/o/oauth2/approval/v2/approvalnativeapp') {
+        $('.qBHUIf').select();
+        document.execCommand('copy');
+        window.close();
+    }
+});
